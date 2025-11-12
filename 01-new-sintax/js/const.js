4ef8acaf66ecd0f60c485c05bb2a194b0b0ec9e6
trianandafajar <mail.trianandafajar@gmail.com>
2025-11-12 13:21:46 +0700

    Update const with objects and arrays in JavaScript

--- 01-new-sintax/js/const.js
+++ 01-new-sintax/js/const.js
@@ -1,9 +1,18 @@
+// Constants in JavaScript
 const PI = 3.14;
 const API_URL = "https://jsonplaceholder.typicode.com";
+
+// Constant object
 const user = { name: "Pojok Code", age: 30 };
+
+// Although 'user' is declared with const, 
+// we can still modify its properties.
 user.age = 31;
 console.log(user);
 
+// Constant array
 const colors = ["red", "green", "blue"];
+
+// We can add new elements to a const array.
 colors.push("yellow");
 console.log(colors);
