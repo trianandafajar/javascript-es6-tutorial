072e6e89d5546d78e46b8b4b4eadbfedde7b6c30
trianandafajar <mail.trianandafajar@gmail.com>
2025-11-13 14:33:12 +0700

    Rewrite iterator examples

--- 07-iterator-generator/js/script.js
+++ 07-iterator-generator/js/script.js
@@ -1,27 +1,30 @@
-// conoth perulangan array di ES5
+// Example of array loop in ES5
 let remarks = ["A", "B", "C"];
 for (let i = 0; i < remarks.length; i++) {
   console.log(remarks[i]);
 }
 
+// Loop using for...of (ES6)
 for (let remark of remarks) {
   console.log(remark);
 }
 
-// Contoh iterator ES6
+// Example of ES6 Iterator
 class Sequence {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   [Symbol.iterator]() {
     let counter = 0;
     let nextIndex = this.start;
+
     return {
       next: () => {
         if (nextIndex <= this.end) {
-          let result = { value: nextIndex, done: false };
+          const result = { value: nextIndex, done: false };
           nextIndex += this.interval;
           counter++;
           return result;
@@ -32,13 +35,13 @@ class Sequence {
   }
 }
 
-let eventNumber = new Sequence(2, 10, 2);
-for (const num of eventNumber) {
+let evenNumbers = new Sequence(2, 10, 2);
+for (const num of evenNumbers) {
   console.log(num);
 }
 
-// mengacess Symbol.iterable secara explicit
-let iterator = eventNumber[Symbol.iterator]();
+// Accessing Symbol.iterator explicitly
+let iterator = evenNumbers[Symbol.iterator]();
 let result = iterator.next();
 
 while (!result.done) {
@@ -46,19 +49,22 @@ while (!result.done) {
   result = iterator.next();
 }
 
+// Iterator with return() method for cleanup
 class Sequence2 {
   constructor(start = 0, end = Infinity, interval = 1) {
     this.start = start;
     this.end = end;
     this.interval = interval;
   }
+
   [Symbol.iterator]() {
     let counter = 0;
     let nextIndex = this.start;
+
     return {
       next: () => {
         if (nextIndex <= this.end) {
-          let result = { value: nextIndex, done: false };
+          const result = { value: nextIndex, done: false };
           nextIndex += this.interval;
           counter++;
           return result;
@@ -66,17 +72,15 @@ class Sequence2 {
         return { value: counter, done: true };
       },
       return: () => {
-        console.log("Clean up ...................");
+        console.log("Cleaning up...");
         return { value: undefined, done: true };
       },
     };
   }
 }
 
-let oddNumber = new Sequence2(1, 10, 2);
-for (const num of oddNumber) {
-  if (num > 7) {
-    break;
-  }
+let oddNumbers = new Sequence2(1, 10, 2);
+for (const num of oddNumbers) {
+  if (num > 7) break;
   console.log(num);
 }
